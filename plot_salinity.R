3a9d9fd24b2b018319c5af367f4531b2cda75e05
sarah salois <sarah.salois@noaa.gov>
2022-05-11 15:48:22 -0400

    salinity data

--- plot_salinity.R
+++ plot_salinity.R
@@ -9,6 +9,183 @@ library(patchwork)
 
 # Note:
 # 1 decibar [dbar] = 1 meter sea water [msw]
+# Inshore
+pa_insh_22 <- read.csv('PIONEER_INSHORE_SURFACE_2022.csv')
+pa_insh_22 <- pa_insh_22[-1,]
+pa_insh_22$date <- ymd_hms(pa_insh_22$time)
+pa_insh_22 <- pa_insh_22 %>%
+  mutate(year = year(date),
+         month = month(date),
+         week = week(date), 
+         day = day(date),
+         date = as.Date(date),
+         density = as.numeric(density), 
+         pressure = as.numeric(pressure),
+         temperature = as.numeric(temperature)) %>%
+  as.data.frame()
+pa_insh_22 <- pa_insh_22 %>%
+  group_by(date) %>%
+  mutate(msal = mean(salinity))
+hist(pa_insh_22$pressure)
+length(unique(pa_insh_22$pressure));mean(pa_insh_22$pressure)
+min(pa_insh_22$pressure);median(pa_insh_22$pressure);max(pa_insh_22$pressure)
+pa_insh_22_ts <- pa_insh_22 %>%
+  dplyr::group_by(year, month, week) %>%
+  dplyr::summarise(msal = mean(na.omit(salinity)), 
+                   mden = mean(density),
+                   mdepth = mean(pressure),
+                   mtmp = mean(temperature))
+
+ann_sal_insh <- pa_insh_22_ts %>%
+  dplyr::group_by(year) %>%
+  summarise(mean_sal = mean(msal))
+
+p1 <- pa_insh_22 %>%
+  ggplot(aes(x = date, y = msal)) +
+  geom_line(lwd = 1.4, color = 'darkblue') +
+  geom_point(aes(fill = 'darkblue'),pch=21,size=3,colour='black') +
+  geom_hline(yintercept = as.numeric(ann_sal_insh[1,2]), 
+             lty = 2, col = 'darkblue', lwd = 1) +
+  labs(title = 'Salinity: Inshore Surface Mooring: Near Surface', 
+       subtitle = paste0('Depth:', round(min(pa_insh_22$pressure),2),':', 
+                         round(max(pa_insh_22$pressure),2), ' M, Mean:', 
+                         round(mean(pa_insh_22$pressure),2), ' M')) +
+  ylab('Salinity') +
+  scale_fill_discrete(guide='none') +
+  theme_minimal()
+
+p2 <- pa_insh_22 %>%
+  ggplot(aes(x = date, y = msal)) +
+  geom_line(lwd = 1.4, color = 'darkblue') +
+  geom_point(aes(fill = 'darkblue'),pch = 21, size = 3,colour = 'black') +
+  geom_hline(yintercept = as.numeric(ann_sal_insh[1,2]), 
+             lty = 2, col = 'darkblue', lwd = 1) +
+  labs(title = 'Week 19, 2022', subtitle ='May 01- 07') +
+  ylab('Salinity') + 
+  xlab('Date') +
+  scale_fill_discrete(guide='none') +
+  scale_x_date(limit=c(as.Date('2022-05-01'), 
+                       as.Date('2022-05-07'))) +
+  theme_minimal()
+
+(plot_spacer() + p2) /  (p1)
+
+# Offshore
+pa_offsh_22 <- read.csv('PIONEER_OFFSHORE_SURFACE_2022.csv')
+pa_offsh_22 <- pa_offsh_22[-1,]
+pa_offsh_22$date <- ymd_hms(pa_offsh_22$time)
+pa_offsh_22 <- pa_offsh_22 %>%
+  mutate(year = year(date),
+         month = month(date),
+         week = week(date), 
+         day = day(date),
+         date = as.Date(date),
+         density = as.numeric(density), 
+         pressure = as.numeric(pressure),
+         temperature = as.numeric(temperature)) %>%
+  as.data.frame()
+pa_offsh_22 <- pa_offsh_22 %>%
+  group_by(date) %>%
+  mutate(msal = mean(salinity))
+hist(pa_offsh_22$pressure)
+length(unique(pa_offsh_22$pressure));mean(pa_offsh_22$pressure)
+min(pa_offsh_22$pressure);median(pa_offsh_22$pressure);max(pa_offsh_22$pressure)
+pa_offsh_22_ts <- pa_offsh_22 %>%
+  dplyr::group_by(year, month, week) %>%
+  dplyr::summarise(msal = mean(na.omit(salinity)), 
+                   mden = mean(density),
+                   mdepth = mean(pressure),
+                   mtmp = mean(temperature))
+
+ann_sal_offsh <- pa_offsh_22_ts %>%
+  dplyr::group_by(year) %>%
+  summarise(mean_sal = mean(msal))
+
+p1 <- pa_offsh_22 %>%
+  ggplot(aes(x = date, y = msal)) +
+  geom_line(lwd = 1.4, color = 'darkblue') +
+  geom_point(aes(fill = 'darkblue'),pch=21,size=3,colour='black') +
+  geom_hline(yintercept = as.numeric(ann_sal_offsh[1,2]), 
+             lty = 2, col = 'darkblue', lwd = 1) +
+  labs(title = 'Salinity: Offshore Surface Mooring: Near Surface', 
+       subtitle = paste0('Depth:', round(min(pa_offsh_22$pressure),2),':', 
+                         round(max(pa_offsh_22$pressure),2), ' M, Mean:', 
+                         round(mean(pa_offsh_22$pressure),2), ' M')) +
+  ylab('Salinity') +
+  scale_fill_discrete(guide='none') +
+  theme_minimal()
+
+p2 <- pa_offsh_22 %>%
+  ggplot(aes(x = date, y = msal)) +
+  geom_line(lwd = 1.4, color = 'darkblue') +
+  geom_point(aes(fill = 'darkblue'),pch = 21, size = 3,colour = 'black') +
+  geom_hline(yintercept = as.numeric(ann_sal_offsh[1,2]), 
+             lty = 2, col = 'darkblue', lwd = 1) +
+  labs(title = 'Week 19, 2022', subtitle ='May 01- 07') +
+  ylab('Salinity') + 
+  xlab('Date') +
+  scale_fill_discrete(guide='none') +
+  scale_x_date(limit=c(as.Date('2022-05-01'), 
+                       as.Date('2022-05-07'))) +
+  theme_minimal()
+
+(plot_spacer() + p2) /  (p1)
+
+# Central 
+pa_22 <- read.csv('PIONEER_PMCO_CTD_2022.csv')
+pa_22 <- pa_22[-1,]
+pa_22$date <- ymd_hms(pa_22$time)
+pa_22 <- pa_22 %>%
+  mutate(year = year(date),
+         month = month(date),
+         week = week(date), 
+         day = day(date),
+         date = as.Date(date),
+         density = as.numeric(density), 
+         pressure = as.numeric(pressure),
+         temperature = as.numeric(temperature)) %>%
+  as.data.frame()
+
+pa_22_ts <- pa_22 %>%
+  dplyr::group_by(date) %>%
+  dplyr::summarise(msal = mean(na.omit(salinity)), 
+                   mden = mean(density),
+                   mdepth = mean(pressure),
+                   mtmp = mean(temperature))
+
+
+p1 <- pa_22_ts %>%
+  ggplot(aes(x = date, y = msal)) +
+  geom_line(lwd = 1.4, color = 'darkblue') +
+  geom_point(aes(fill = 'darkblue'),pch=21,size=3,colour='black') +
+  geom_hline(yintercept = as.numeric(ann_sal[2,2]), 
+             lty = 2, col = 'darkblue', lwd = 1) +
+  labs(title = 'Salinity: Central Offshore Profiler Mooring: Wire-Following Profiler', 
+       subtitle = paste0('Depth:', round(min(pa_22$pressure),2),':', 
+                         round(max(pa_22$pressure),2), ' M, Mean:', 
+                         round(mean(pa_22$pressure),2), ' M')) +  
+  ylab('Salinity') + 
+  scale_fill_discrete(guide='none') +
+  theme_minimal()
+
+
+p2 <- pa_22_ts %>%
+  ggplot(aes(x = date, y = msal)) +
+  geom_line(lwd = 1.4, color = 'darkblue') +
+  geom_point(aes(fill = 'darkblue'),pch = 21, size = 3,colour = 'black') +
+  geom_hline(yintercept = as.numeric(ann_sal[2,2]), 
+             lty = 2, col = 'darkblue', lwd = 1) +
+  labs(title = 'Week 19, 2022', subtitle ='May 01- 07') +
+  ylab('Salinity') + 
+  xlab('Date') +
+  scale_fill_discrete(guide='none') +
+  scale_x_date(limit=c(as.Date('2022-05-01'), 
+                       as.Date('2022-05-07'))) +
+  theme_minimal()
+
+(plot_spacer() + p2) /  (p1)
+
+##### --- extra ---------- #####
 pa_21 <- read.csv('PIONEER_PMCO_CTD_2021.csv')
 pa_21$date <- ymd_hms(pa_21$time)
 pa_21 <- pa_21[-1,]
@@ -22,7 +199,6 @@ pa_21 <- pa_21 %>%
          pressure = as.numeric(pressure),
          temperature = as.numeric(temperature)) %>%
   as.data.frame()
-
 
 pa_21_ts <- pa_21 %>%
   dplyr::group_by(date) %>%
@@ -172,16 +348,16 @@ p1 <- pa_22_ts %>%
   geom_point(aes(fill = 'darkblue'),pch=21,size=3,colour='black') +
   geom_hline(yintercept = as.numeric(ann_sal[2,2]), 
              lty = 2, col = 'darkblue', lwd = 1) +
-  ggtitle('Salinity Time Series') +
+  labs(title = 'Salinity: Central Offshore Profiler Mooring: Wire-Following Profiler', 
+       subtitle = paste0('Depth:', round(min(pa_22$pressure),2),':', 
+                         round(max(pa_22$pressure),2), ' M, Mean:', 
+                         round(mean(pa_22$pressure),2), ' M')) +  
   ylab('Salinity') + 
-  #labs(fill = 'Year', color = 'Year') +
- #guides(fill=FALSE) +
   scale_fill_discrete(guide='none') +
   theme_minimal()
 
   
 p2 <- pa_22_ts %>%
-  #dplyr::filter(week %in% 19) %>%
   ggplot(aes(x = date, y = msal)) +
   geom_line(lwd = 1.4, color = 'darkblue') +
   geom_point(aes(fill = 'darkblue'),pch = 21, size = 3,colour = 'black') +
@@ -190,8 +366,6 @@ p2 <- pa_22_ts %>%
   labs(title = 'Week 19, 2022', subtitle ='May 01- 07') +
   ylab('Salinity') + 
   xlab('Date') +
-  #labs(fill = 'Year', color = 'Year') +
-  #guides(fill=FALSE) +
   scale_fill_discrete(guide='none') +
   scale_x_date(limit=c(as.Date('2022-05-01'), 
                        as.Date('2022-05-07'))) +
