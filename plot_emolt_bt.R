f98b51ee04c692d6ba6949709bbb59660fbfae4f
sarah salois <sarah.salois@noaa.gov>
2022-07-01 11:04:53 -0400

    updates

--- plot_emolt_bt.R
+++ plot_emolt_bt.R
@@ -46,6 +46,16 @@ emolt <- emolt %>%
          day = day(datet)) %>%
   as.data.frame()
 
+e_202226 <- emolt %>% filter(year == 2022 & week == 26)
+e_202126 <- emolt %>% filter(year == 2021 & week == 26)
+
+e_202225 <- emolt %>% filter(year == 2022 & week == 25)
+e_202125 <- emolt %>% filter(year == 2021 & week == 25)
+
+
+e_202224 <- emolt %>% filter(year == 2022 & week == 24)
+e_202124 <- emolt %>% filter(year == 2021 & week == 24)
+
 e_202223 <- emolt %>% filter(year == 2022 & week == 23)
 e_202123 <- emolt %>% filter(year == 2021 & week == 23)
 
@@ -53,43 +63,53 @@ e_202222 <- emolt %>% filter(year == 2022 & week == 22)
 e_202122 <- emolt %>% filter(year == 2021 & week == 22)
 
 # scale temps
-# scale temps
+range(e_202226$mean_temp) # 
+range(e_202126$mean_temp) # min,max
+
+range(e_202225$mean_temp) # 
+range(e_202125$mean_temp) # min,max
+
+range(e_202224$mean_temp) # max
+range(e_202124$mean_temp) # min
+
 range(e_202223$mean_temp) # max
 range(e_202123$mean_temp) # min
 range(e_202222$mean_temp) # max
 range(e_202122$mean_temp) # min
+blues2 <- blues[c(5,7,9,11,13,24)]
 
 # Plot
 p1 <- ggplot() + 
   geom_contour_filled(data = atl,
                       aes(x=x,y=y,z=-1*z),
-                      breaks=c(0,50,100,250,500,Inf),
+                      breaks=c(0,50,100,200,500,Inf),
                       size=c(0.3)) +
-  scale_fill_manual(values = blues[5:25], # 5:20
+  scale_fill_manual(values = blues2, # 5:20 blues[5:25]
                     #name = paste("Depth (m)"),
                     #labels = depths, 
                     guide = 'none') +
   geom_contour(data = atl,
                aes(x=x,y=y,z=-1*z),
-               breaks=c(0,50,100,250,500,Inf),
+               breaks=c(0,50,100,200,500,Inf),
                size=c(0.3),
-               col = 'lightgrey') +
+               col = 'darkgrey') +
   # NAFO subareas
   geom_sf(data=nafo %>% st_as_sf(), fill = NA, colour = 'Black') +
   new_scale_fill() +
   # Bottom data as points
-  geom_point(data = e_202223,
+  geom_point(data = e_202226,
              aes(x = lon, y = lat, color = mean_temp), 
              size = 3) +
   scale_color_gradient(low = "darkblue", 
                        high = "darkred", 
-                       limits = c(min(e_202123$mean_temp), max(e_202223$mean_temp)),
+                       limits = c(min(e_202126$mean_temp),
+                                  max(e_202126$mean_temp)),
                        guide = 'none') +
   geom_polygon(data = coast, aes(x=long, y = lat, group = group), 
                color = "gray20", fill = "wheat3", alpha = 0.7) +
   coord_sf(xlim = c(-76,-65), ylim = c(35,45),datum = sf::st_crs(4326)) + 
   labs(title = '2022 Mean Bottom Temperature ',
-       subtitle = 'Week 23') +
+       subtitle = 'Week 26') +
   scale_fill_discrete(guide = 'none') +
   annotation_scale(location = "tl", width_hint = 0.5) +
   annotation_north_arrow(location = "tl",
@@ -97,7 +117,7 @@ p1 <- ggplot() +
                          pad_x = unit(0.75, "in"),
                          pad_y = unit(0.5, "in"),
                          style = north_arrow_fancy_orienteering) +
-  theme_minimal() 
+  theme_bw()
 
 
 p2 <- ggplot() + 
@@ -105,9 +125,10 @@ p2 <- ggplot() +
                       aes(x=x,y=y,z=-1*z),
                       breaks=c(0,50,100,250,500,Inf),
                       size=c(0.3)) +
-  scale_fill_manual(values = blues[5:25], # 5:20
+  scale_fill_manual(values = blues2, # 5:20
                     name = paste("Depth (m)"),
-                    labels = depths) +
+                    labels = depths, 
+                    position = 'bottom') +
   geom_contour(data = atl,
                aes(x=x,y=y,z=-1*z),
                breaks=c(0,50,100,250,500,Inf),
@@ -117,22 +138,27 @@ p2 <- ggplot() +
   geom_sf(data=nafo %>% st_as_sf(), fill = NA, colour = 'Black') +
   new_scale_fill() +
   # Bottom data as points
-  geom_point(data = e_202123,
+  geom_point(data = e_202126,
              aes(x = lon, y = lat, color = mean_temp),
              size = 3) + # alpha = 0.5
   scale_color_gradient(low = "darkblue", 
                        high = "darkred",
-                       limits = c(min(e_202123$mean_temp), max(e_202223$mean_temp)),
+                       limits = c(min(e_202126$mean_temp), 
+                                  max(e_202126$mean_temp)),
                        name = paste('Temperature (°C)')) +
   geom_polygon(data = coast, aes(x=long, y = lat, group = group), 
                color = "gray20", fill = "wheat3", alpha = 0.7)+
   coord_sf(xlim = c(-76,-65), ylim = c(35,45),datum = sf::st_crs(4326)) + 
   labs(title = '2021 Mean Bottom Temperature',
-       subtitle = 'Week 23') +
+       subtitle = 'Week 26') +
+  xlab('Longitude') +
+  ylab('Latitude') +
   theme(legend.position = 'bottom', 
         legend.box = "horizontal", 
         legend.margin = margin()) +
-  theme_minimal()
+  theme_bw()
 
+p2 + scale_fill_continuous(guide = guide_legend()) +
+  theme(legend.position="bottom")
 # plot
 p1 | p2 
